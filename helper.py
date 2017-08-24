49085317426144ba17e52c5750a3d69fa122ecfd
Erbs <uidg5371@contiwan.com>
2017-08-24 21:18:39 +0200

    add data augmentation

--- helper.py
+++ helper.py
@@ -10,6 +10,9 @@ import tensorflow as tf
 from glob import glob
 from urllib.request import urlretrieve
 from tqdm import tqdm
+from sklearn.metrics import jaccard_similarity_score
+import cv2
+from skimage import transform
 
 
 class DLProgress(tqdm):
@@ -57,6 +60,38 @@ def maybe_download_pretrained_vgg(data_dir):
         # Remove zip file to save space
         os.remove(os.path.join(vgg_path, vgg_filename))
 
+def maybe_download_kitti(data_dir):
+    """
+    Download and extract kitti road dataset if it doesn't exist
+    :param data_dir: Directory to download the data to
+    """
+    kitti_filename = 'data_road.zip'
+    kitti_path = os.path.join(data_dir, 'data_road')
+   
+    if not os.path.exists(kitti_path):
+        # Clean kitti dir
+        if os.path.exists(kitti_path):
+            shutil.rmtree(kitti_path)
+        os.makedirs(kitti_path)
+
+        # Download kitti road dataset
+        print('Downloading kitti road dataset...')
+        with DLProgress(unit='B', unit_scale=True, miniters=1) as pbar:
+            urlretrieve(
+                'http://kitti.is.tue.mpg.de/kitti/data_road.zip',
+                os.path.join(kitti_path, kitti_filename),
+                pbar.hook)
+
+        # Extract dataset
+        print('Extracting dataset...')
+        zip_ref = zipfile.ZipFile(os.path.join(kitti_path, kitti_filename), 'r')
+        zip_ref.extractall(data_dir)
+        zip_ref.close()
+
+        # Remove zip file to save space
+        os.remove(os.path.join(kitti_path, kitti_filename))
+    else:
+        print('Found kitti dataset!')
 
 def gen_batch_function(data_folder, image_shape):
     """
@@ -71,10 +106,10 @@ def gen_batch_function(data_folder, image_shape):
         :param batch_size: Batch Size
         :return: Batches of training data
         """
-        image_paths = glob(os.path.join(data_folder, 'image_2', '*.png'))
+        image_paths = glob(os.path.join(data_folder, 'images', '*.png'))
         label_paths = {
             re.sub(r'_(lane|road)_', '_', os.path.basename(path)): path
-            for path in glob(os.path.join(data_folder, 'gt_image_2', '*_road_*.png'))}
+            for path in glob(os.path.join(data_folder, 'labels', '*_road_*.png'))}
         background_color = np.array([255, 0, 0])
 
         random.shuffle(image_paths)
@@ -96,7 +131,6 @@ def gen_batch_function(data_folder, image_shape):
 
             yield np.array(images), np.array(gt_images)
     return get_batches_fn
-
 
 def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape):
     """
@@ -124,8 +158,17 @@ def gen_test_output(sess, logits, keep_prob, image_pl, data_folder, image_shape)
 
         yield os.path.basename(image_file), np.array(street_im)
 
-
 def save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image):
+    """
+    Generate test output using the test images
+    :param runs_dir: Directory to store the results to
+    :param data_dir: Directory to download the data to
+    :param sess: TF session
+    :param image_shape: Tuple - Shape of image
+    :param logits: TF Tensor for the logits
+    :param keep_prob: TF Placeholder for the dropout keep robability
+    :param input_image: TF Placeholder for the image placeholder
+    """
     # Make folder for current run
     output_dir = os.path.join(runs_dir, str(time.time()))
     if os.path.exists(output_dir):
@@ -138,3 +181,208 @@ def save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_p
         sess, logits, keep_prob, input_image, os.path.join(data_dir, 'data_road/testing'), image_shape)
     for name, image in image_outputs:
         scipy.misc.imsave(os.path.join(output_dir, name), image)
+
+    # Save graph
+    model_dir = os.path.join(output_dir, 'model')
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    tf.train.write_graph(sess.graph, model_dir, 'graph.pb', as_text=False)
+
+    # Save weights
+    saver = tf.train.Saver()
+    model_path = os.path.join(model_dir, 'model.ckpt')
+    save_path = saver.save(sess, model_path)
+    print('Saved model to: {}'.format(save_path))
+
+def augment_data(data_dir, image_shape):
+    """Augument data by applying various image transformations
+    """
+    center     = (image_shape[1]/ 2, image_shape[0]/ 2)
+    images     = glob(os.path.join(data_dir, 'images', '*.png'))
+    num_images = len(images)
+
+    # GT 
+    label_paths = {
+        re.sub(r'_(lane|road)_', '_', os.path.basename(path)): path
+        for path in glob(os.path.join(data_dir, 'labels', '*_road_*.png'))}
+
+    print('Augmenting training data...')
+    for i in tqdm(range(500)):
+        rand_idx   = random.randint(0, num_images-1)
+        image_file = images[rand_idx]
+        image      = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+
+        new_angle   = random.uniform(-15.0, 15.0)
+        new_shift_x = random.uniform(-10.0, 10.0)
+        new_shift_y = random.uniform(-10.0, 10.0)
+        new_scale_x = random.uniform(0.9, 1.1)
+        new_scale_y = random.uniform(0.9, 1.1)
+        #new_shear   = random.uniform(-10.0/60, 10.0/60) #rad!
+
+        
+        M_rot     = cv2.getRotationMatrix2D(center,new_angle,1)
+        M_shift   = np.float32([[1,0,new_shift_x],[0,1,new_shift_y]])
+        M_scale   = np.float32([[new_scale_x,0,center[0]*(1.0-new_scale_x)],[0,new_scale_y,center[1]*(1.0-new_scale_y)]])
+        #M_shear = transform.AffineTransform(shear=new_shear)
+        
+        new_image = cv2.warpAffine(image,     M_rot,  image_shape[::-1])
+        new_image = cv2.warpAffine(new_image, M_scale,image_shape[::-1])
+        new_image = cv2.warpAffine(new_image, M_shift,image_shape[::-1])
+        #new_image = transform.warp(new_image, inverse_map=M_shear)
+
+        # Maybe flip image vertically
+        percentage_chance = 0.5
+        flipped           = False
+        if random.random() < percentage_chance:
+            new_image = cv2.flip(new_image,1)
+            flipped   = True
+
+        # Save new image
+        output_dir = os.path.join(data_dir, 'images')
+        new_name   = 'augment_' + str(i) + '.png'
+        scipy.misc.imsave(os.path.join(output_dir, new_name), new_image)
+
+        # Get the corresponding gt data
+        gt_image_file = label_paths[os.path.basename(image_file)]
+        gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
+
+        new_gt = cv2.warpAffine(gt_image,  M_rot,  image_shape[::-1], borderValue=(255,0,0))
+        new_gt = cv2.warpAffine(new_gt,    M_scale,image_shape[::-1], borderValue=(255,0,0))
+        new_gt = cv2.warpAffine(new_gt,    M_shift,image_shape[::-1], borderValue=(255,0,0))
+
+        if flipped:
+            new_gt = cv2.flip(new_gt,1)
+
+        # Save new gt image
+        output_dir = os.path.join(data_dir, 'labels')
+        new_name   = 'augment_road_' + str(i) + '.png'
+        scipy.misc.imsave(os.path.join(output_dir, new_name), new_gt)
+
+    print('Augmentation done!')
+
+
+def split_test_validation(data_folder, test_size):
+    """
+    Split the training data into training and validation part
+    :param data_folder: Path to the folder that contains the datasets
+    :param test_size:   Fraction of training data which shall be used for validation
+    """
+    # Split training dataset to training and validation part
+    print('Split training dataset in training and validation dataset ...')
+
+    # Get paths
+    image_paths = glob(os.path.join(data_folder, 'image_2', '*.png'))
+    label_paths = {
+            re.sub(r'_(lane|road)_', '_', os.path.basename(path)): path
+            for path in glob(os.path.join(data_folder, 'gt_image_2', '*_road_*.png'))}
+
+    # Select images for train and validation dataset
+    n_all       = len(image_paths)
+    all_idx     = list(range(n_all))
+
+    random.seed(42)
+    random.shuffle(all_idx)
+
+    n_valid   = int(n_all*test_size)
+    train_idx = all_idx[n_valid:]
+    valid_idx = all_idx[:n_valid]
+
+    # Create folder for training images
+    output_dir_img = os.path.join(data_folder,     'train')
+    output_dir_img = os.path.join(output_dir_img,  'images')
+    if os.path.exists(output_dir_img):
+        shutil.rmtree(output_dir_img)
+    os.makedirs(output_dir_img)
+
+    # Create folder for training labels
+    output_dir_labels = os.path.join(data_folder,         'train')
+    output_dir_labels = os.path.join(output_dir_labels,  'labels')
+    if os.path.exists(output_dir_labels):
+        shutil.rmtree(output_dir_labels)
+    os.makedirs(output_dir_labels) 
+    
+    # Copy images and labels to new location
+    for i in train_idx:
+        src = image_paths[i]
+        dst = os.path.join(output_dir_img, os.path.basename(src))
+        shutil.copyfile(src, dst)
+
+        gt_image_file = label_paths[os.path.basename(src)]
+        src = gt_image_file
+        dst = os.path.join(output_dir_labels, os.path.basename(src))
+        shutil.copyfile(src, dst) 
+
+    # Create folder for validation images
+    output_dir_img = os.path.join(data_folder,     'valid')
+    output_dir_img = os.path.join(output_dir_img,  'images')
+    if os.path.exists(output_dir_img):
+        shutil.rmtree(output_dir_img)
+    os.makedirs(output_dir_img)
+
+    # Create folder for training labels
+    output_dir_labels = os.path.join(data_folder,        'valid')
+    output_dir_labels = os.path.join(output_dir_labels,  'labels')
+    if os.path.exists(output_dir_labels):
+        shutil.rmtree(output_dir_labels)
+    os.makedirs(output_dir_labels) 
+    
+    # Copy images and labels to new location
+    for i in valid_idx:
+        src = image_paths[i]
+        dst = os.path.join(output_dir_img, os.path.basename(src))
+        shutil.copyfile(src, dst)
+
+        gt_image_file = label_paths[os.path.basename(src)]
+        src = gt_image_file
+        dst = os.path.join(output_dir_labels, os.path.basename(src))
+        shutil.copyfile(src, dst) 
+
+    print('Dataset split done!')
+
+def test_nn(data_dir, sess, image_shape, logits, 
+            keep_prob, image_pl, num_classes):
+    """
+    Calculate and print mean jaccard index to measure validation accuracy
+    :param data_dir: Directory to download the data to
+    :param sess: TF session
+    :param image_shape: Tuple - Shape of image
+    :param logits: TF Tensor for the logits
+    :param keep_prob: TF Placeholder for the dropout keep robability
+    :param image_pl: TF Placeholder for the image placeholder
+    :param num_classes: Number of classes for classification
+    """
+
+    all_iou = 0
+    num_iou = 0
+    label_paths = {
+            re.sub(r'_(lane|road)_', '_', os.path.basename(path)): path
+            for path in glob(os.path.join(data_dir, 'labels', '*_road_*.png'))}
+
+    bg_color = np.array([255, 0, 0])
+
+    for image_file in glob(os.path.join(data_dir, 'images', '*.png')):
+        image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+
+        im_softmax = sess.run(
+            [tf.nn.softmax(logits)],
+            {keep_prob: 1.0, image_pl: [image]})
+
+        # Get prediction from nn
+        im_softmax = im_softmax[0][:, 1].reshape(image_shape[0], image_shape[1])
+        segmentation = (im_softmax > 0.5).reshape(image_shape[0], image_shape[1], 1)
+        
+        # Get gt data
+        gt_image_file = label_paths[os.path.basename(image_file)]
+        gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
+
+        gt_road = np.all(gt_image == bg_color, axis=2)
+        gt_road = np.logical_not(gt_road)
+
+        # Calculate iou
+        iou = jaccard_similarity_score(gt_road, np.squeeze(segmentation,axis=2))
+
+        all_iou += iou
+        num_iou += 1
+
+    all_iou /= num_iou
+    print("mean iou: {}".format(all_iou))
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import helper
 import warnings
 from distutils.version import LooseVersion
 import project_tests as tests
-
+from tqdm import tqdm
 
 # Check TensorFlow Version
 assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
@@ -26,14 +26,25 @@ def load_vgg(sess, vgg_path):
     """
     # TODO: Implement function
     #   Use tf.saved_model.loader.load to load the model and weights
-    vgg_tag = 'vgg16'
-    vgg_input_tensor_name = 'image_input:0'
-    vgg_keep_prob_tensor_name = 'keep_prob:0'
+    vgg_tag                    = 'vgg16'
+    vgg_input_tensor_name      = 'image_input:0'
+    vgg_keep_prob_tensor_name  = 'keep_prob:0'
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    return None, None, None, None, None
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    
+    # Extract layers of vgg 
+    vgg_graph  = tf.get_default_graph()
+    vgg_input  = vgg_graph.get_tensor_by_name(vgg_input_tensor_name)
+    vgg_keep   = vgg_graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    vgg_layer3 = vgg_graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    vgg_layer4 = vgg_graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    vgg_layer7 = vgg_graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+    
+    return vgg_input, vgg_keep, vgg_layer3, vgg_layer4, vgg_layer7
+    
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,7 +58,29 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+    vgg_layer7 = tf.layers.conv2d(vgg_layer7_out, num_classes, kernel_size=1, name='vgg_layer7')
+
+    vgg_layer4 = tf.layers.conv2d(vgg_layer4_out, num_classes, kernel_size=1, name='vgg_layer4')
+
+    vgg_layer3 = tf.layers.conv2d(vgg_layer3_out, num_classes, kernel_size=1, name='vgg_layer3')
+     
+    # Add transpose layer                
+    up_layer1 = tf.layers.conv2d_transpose(vgg_layer7, num_classes, kernel_size=4, strides=(2,2), padding='same', name='decoder_layer1')
+
+    # Add skip layer
+    skip1 = tf.add(vgg_layer4, up_layer1, name='skip_layer1')      
+
+    # Add transpose layer                
+    up_layer2 = tf.layers.conv2d_transpose(skip1, num_classes, kernel_size=4, strides=(2,2), padding='same', name='decoder_layer2')
+
+    # Add skip layer
+    skip2 = tf.add(up_layer2, vgg_layer3, name='skip_layer2')
+
+    # Add transpose layer                
+    out_layer = tf.layers.conv2d_transpose(skip2, num_classes, kernel_size=16, strides=(8,8), padding='same', name='decoder_layer3')      
+
+    return out_layer
+
 tests.test_layers(layers)
 
 
@@ -61,7 +94,15 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+    # 4d to 2d
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    y = tf.reshape(correct_label, (-1, num_classes))
+
+    # now define a loss function and a trainer/optimizer
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
@@ -81,39 +122,99 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-    pass
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.local_variables_initializer())
+
+    for i in tqdm(range(epochs)):
+        train_loss  = 0
+        num_samples = 0
+        print("Train epoch: {}".format(i+1))
+
+        # Train
+        for X, y in get_batches_fn(batch_size):
+                num_samples += len(X)
+                loss, _ = sess.run(
+                    [cross_entropy_loss, train_op],
+                    feed_dict={input_image: X, correct_label: y, keep_prob: 0.8})
+                train_loss += loss
+
+        # Calc loss
+        train_loss /= num_samples
+        print("train_loss: {}".format(train_loss))
+
 tests.test_train_nn(train_nn)
 
-
 def run():
-    num_classes = 2
-    image_shape = (160, 576)
-    data_dir = './data'
-    runs_dir = './runs'
+    # run parameters
+    num_classes   = 2
+    valid_size    = 0.2
+    image_shape   = (160, 576)
+    data_dir      = './data'
+    runs_dir      = './runs'
+    
+    # training parameters
+    epochs        = 25
+    batch_size    = 1
+    learning_rate = tf.constant(0.0001)
+    train_network = False
+    load_cp       = True
+    augment_data  = False
+    validate_nn   = True
+    save_images   = False
+
+    # Download kitti dataset
+    helper.maybe_download_kitti(data_dir)
     tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
     helper.maybe_download_pretrained_vgg(data_dir)
+
+    # Split training/validation data
+    helper.split_test_validation(os.path.join(data_dir, 'data_road/training'), valid_size)
+
+    # Augment training data
+    if augment_data:
+        helper.augment_data(os.path.join(data_dir, 'data_road/training/train'), image_shape)
 
     # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
     with tf.Session() as sess:
+
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
-        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training/train'), image_shape)
 
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
+        vgg_input, keep_prob, vgg_layer3, vgg_layer4, vgg_layer7 = load_vgg(sess, vgg_path)
+        out_layer = layers(vgg_layer3, vgg_layer4, vgg_layer7, num_classes)
+
+        correct_label = tf.placeholder(tf.float32, [None, image_shape[0], image_shape[1], num_classes])
+        logits, train_op, cross_entropy_loss = optimize(out_layer, correct_label, learning_rate, num_classes)
 
         # TODO: Train NN using the train_nn function
+        if train_network:
+            train_nn(sess, epochs, batch_size, get_batches_fn, train_op,
+                cross_entropy_loss, vgg_input, correct_label, keep_prob,
+                learning_rate)
+        elif load_cp:
+            saver = tf.train.Saver()
+            saver.restore(sess, 'runs/1503370650.1891687/model/model.ckpt')
+
+        # Test neural network
+        if validate_nn:
+            helper.test_nn(os.path.join(data_dir, 
+                'data_road/training/valid'), sess, 
+                image_shape, logits, keep_prob, vgg_input, num_classes) 
 
         # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        if save_images:
+            helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, vgg_input)
 
         # OPTIONAL: Apply the trained model to a video
 
